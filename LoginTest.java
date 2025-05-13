0960beb0921791d7d828f07b0fb3e3356c768730
Vibinanthony <vibinfer9003@gmail.com>
2025-05-13 16:01:47 +0530

    Sample test case for login using Selenium+Java

---
+++ LoginTest.java
@@ -0,0 +1,30 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LoginTest {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "path/to/chromedriver");
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://example.com/login");
+
+        WebElement username = driver.findElement(By.id("username"));
+        WebElement password = driver.findElement(By.id("password"));
+        WebElement loginBtn = driver.findElement(By.id("loginBtn"));
+
+        username.sendKeys("testuser");
+        password.sendKeys("password123");
+        loginBtn.click();
+
+        String expectedUrl = "https://example.com/dashboard";
+        if (driver.getCurrentUrl().equals(expectedUrl)) {
+            System.out.println("Login Test Passed");
+        } else {
+            System.out.println("Login Test Failed");
+        }
+
+        driver.quit();
+    }
+}
